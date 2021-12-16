43694865d2b494c2b28761ea01edcd1e6aaab7cb
jlennartz <jlen@uni-bonn.de>
2021-12-16 20:49:11 +0100

    initial commit for model

---
+++ src/model.py
@@ -0,0 +1,29 @@
+from typing import Dict, Iterable, Callable, Generator, Union
+
+from layer import *
+
+class DualBranchAE(nn.Module):
+    def __init__(self, encoder, decoder, in_size, n_classes=None, thresholds='learned'):
+        super().__init__()
+        
+        if encoder == 'dual':
+            self.encoder = DualLinkEncoder(in_size)
+            
+        elif encoder == 'single':
+            self.encoder = SingleLinkEncoder(in_size) 
+            
+        elif encoder == 'none':
+            self.encoder = ZeroLinkEncoder(in_size)
+        
+        if decoder == 'reconstruction':
+            self.decoder = ReconstructionDecoder()
+            
+        elif decoder == 'segmentation':
+            self.decoder = SegmentationDecoder(num_classes=5, thresholds=thresholds)
+            
+            
+    def forward(self, x):
+        x_encoded = self.encoder(x)
+        x_decoded = self.decoder(x_encoded)
+        
+        return x_decoded
