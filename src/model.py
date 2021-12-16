e5903c09b05e97f1b9ada7f131b3a0371bef9ec2
jlennartz <jlen@uni-bonn.de>
2021-12-16 20:51:09 +0100

    small fix in model

--- src/model.py
+++ src/model.py
@@ -5,25 +5,25 @@ from layer import *
 class DualBranchAE(nn.Module):
     def __init__(self, encoder, decoder, in_size, n_classes=None, thresholds='learned'):
         super().__init__()
-        
+
         if encoder == 'dual':
             self.encoder = DualLinkEncoder(in_size)
-            
+
         elif encoder == 'single':
-            self.encoder = SingleLinkEncoder(in_size) 
-            
-        elif encoder == 'none':
+            self.encoder = SingleLinkEncoder(in_size)
+
+        elif encoder == 'zero':
             self.encoder = ZeroLinkEncoder(in_size)
-        
+
         if decoder == 'reconstruction':
             self.decoder = ReconstructionDecoder()
-            
+
         elif decoder == 'segmentation':
             self.decoder = SegmentationDecoder(num_classes=5, thresholds=thresholds)
-            
-            
+
+
     def forward(self, x):
         x_encoded = self.encoder(x)
         x_decoded = self.decoder(x_encoded)
-        
+
         return x_decoded
